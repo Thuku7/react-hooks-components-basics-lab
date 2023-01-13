6785e6e33e8aabff735c18873cacc26a44e3bb79
Robert <robethuku11@gmail.com>
2023-01-13 12:53:05 +0300

    Add the home and about components to the app component

--- src/components/App.js
+++ src/components/App.js
@@ -30,8 +30,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About/>
     </div>
   );
 }
